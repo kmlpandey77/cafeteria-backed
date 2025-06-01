ca851d5d79604ca039ace8d43b0d5c766b6cf0d4
Kamal Pandey <kamal.pandey@midastechnologies.com.np>
2025-06-01 14:04:13 +0545

    user add/edit

--- app/Filament/System/Resources/OrganizationResource/RelationManagers/UsersRelationManager.php
+++ app/Filament/System/Resources/OrganizationResource/RelationManagers/UsersRelationManager.php
@@ -2,11 +2,13 @@
 
 namespace App\Filament\System\Resources\OrganizationResource\RelationManagers;
 
+use App\Enums\UserRole;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\Hash;
 
 class UsersRelationManager extends RelationManager
 {
@@ -19,15 +21,37 @@ class UsersRelationManager extends RelationManager
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('role')
+                    ->options(fn () => collect(UserRole::cases())
+                        ->filter(fn ($role) => $role !== UserRole::SuperAdmin)
+                        ->mapWithKeys(fn ($role) => [$role->value => $role->name])
+                        ->toArray())
+                    ->required()
+                    ->enum(UserRole::class),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->dehydrateStateUsing(fn ($state) => $state ? Hash::make($state) : null)
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn (string $operation): bool => $operation === 'create')
+                    ->maxLength(255),
             ]);
     }
 
     public function table(Table $table): Table
     {
         return $table
+            ->modifyQueryUsing(fn ($query) => $query->where('role', '!=', UserRole::SuperAdmin->value))
             ->recordTitleAttribute('name')
             ->columns([
                 Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('role')
+                    ->badge()
+                    ->formatStateUsing(fn ($state) => UserRole::from($state)->name),
             ])
             ->filters([
                 //
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'org_id',
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
